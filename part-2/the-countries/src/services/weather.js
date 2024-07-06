a93e765aabb02fb17d4dc9abb143b2f056e5d379
dev-shahed <shahedthedev@gmail.com>
2024-07-07 02:19:30 +0600

    2.20*: Data for countries, step 3 done

--- part-2/the-countries/src/services/weather.js
+++ part-2/the-countries/src/services/weather.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 const baseUrl = "https://api.openweathermap.org/data/2.5/forecast";
 
-const apiKey = "eaebd89828e1c9c4d93a81aaecbbe540";
+const apiKey = import.meta.env.VITE_API_KEY;
+console.log(apiKey);
 
 const getData = async (lat, lon) => {
   const request = axios.get(`${baseUrl}?lat=${lat}&lon=${lon}&appid=${apiKey}`);
